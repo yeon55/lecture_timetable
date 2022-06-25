6a006a63ea5797b8141a0a34c7012608466a4934
SEOYEON HAN <tjdus0824@naver.com>
2022-06-25 22:39:08 +0900

    ADD: 강의 종료 시간 입력필드 생성 및 에러처리 작업

--- Desktop/timetable/src/InputModal/InputModal.jsx
+++ Desktop/timetable/src/InputModal/InputModal.jsx
@@ -12,7 +12,12 @@ import {
   FormControlLabel,
   FormLabel,
   Radio,
+  Stack,
+  MenuItem,
 } from "@mui/material";
+const timeOptions = new Array(12)
+  .fill(null)
+  .map((e, i) => ({ value: i + 9, label: i + 9 }));
 
 function InputModal({ showModal, handleClose }) {
   const {
@@ -42,7 +47,7 @@ function InputModal({ showModal, handleClose }) {
             )}
           />
           {errors.lectureName?.type === "required" && (
-            <p style={{ color: "red" }}>강의명을 입력해주세요.</p>
+            <p style={{ color: "#d32f2f" }}>강의명을 입력해주세요.</p>
           )}
 
           <FormControl style={{ marginTop: "30px" }}>
@@ -82,6 +87,65 @@ function InputModal({ showModal, handleClose }) {
               )}
             />
           </FormControl>
+          <Stack spacing={3} style={{ marginTop: "30px", width: "350px" }}>
+            <Controller
+              control={control}
+              name="startTime"
+              rules={{ required: true }}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  select
+                  error={!!errors.startTime}
+                  style={{ marginTop: "30px", width: "350px" }}
+                  label="시작 시간"
+                  placeholder="시작 시간 선택"
+                >
+                  {timeOptions.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              )}
+            />
+            {errors.startTime?.type === "required" && (
+              <p style={{ color: "#d32f2f" }}>강의 시작 시간 선택</p>
+            )}
+
+            <Controller
+              control={control}
+              name="endTime"
+              rules={{
+                required: true,
+                validate: (value) => getValues("startTime") < value,
+              }}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  select
+                  error={!!errors.endTime}
+                  style={{ marginTop: "30px", width: "350px" }}
+                  label="종료 시간"
+                  placeholder="종료 시간 선택"
+                >
+                  {timeOptions.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              )}
+            />
+            {errors.endTime?.type === "required" && (
+              <p style={{ color: "#d32f2f" }}>강의 종료 시간 선택</p>
+            )}
+            {errors.endTime?.type === "validate" && (
+              <p style={{ color: "#d32f2f" }}>
+                시작시간과 종료시간을 확인해주세요.
+              </p>
+            )}
+          </Stack>
         </DialogContent>
         <DialogActions>
           <Button>취소</Button>
